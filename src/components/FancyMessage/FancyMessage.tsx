533d3dae40d1f3d53374c93b5bc3d77c742967ef
Michael de Lima Alves <michaelalves@outlook.com>
2019-01-24 19:15:03 -0200

    add a react component

---
+++ src/components/FancyMessage/FancyMessage.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class FancyMessage extends React.Component<any, any> {
+  public render() {
+    return <h5>Hey You!</h5>;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Greeter } from './helpers/Greeter';
 export { default as Bye } from './helpers/Bye';
+export { default as FancyMessage } from './components/FancyMessage/FancyMessage';
