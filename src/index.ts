7e37aa05e8dbc67f7eca957bcc8daf416fe9c05c
Michael de Lima Alves <michaelalves@outlook.com>
2019-01-24 20:54:26 -0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Greeter } from './helpers/Greeter';
 export { default as Bye } from './helpers/Bye';
 export { default as FancyMessage } from './components/FancyMessage/FancyMessage';
+export { default as Button } from './components/Button/Button';
